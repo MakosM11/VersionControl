e1c1a27c784720b91f21f3c3dc0de7a1b2ebb125
MakosM11 <mark.makos@stud.uni-corvinus.hu>
2022-09-30 11:00:22 +0200

    Commit feature

--- UserMaintenance/UserMaintenance/Resource1.Designer.cs
+++ UserMaintenance/UserMaintenance/Resource1.Designer.cs
@@ -70,7 +70,7 @@ namespace UserMaintenance {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Keesztnév.
+        ///   Looks up a localized string similar to Keresztnév.
         /// </summary>
         internal static string FirstName {
             get {
