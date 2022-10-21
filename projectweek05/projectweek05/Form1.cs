3a03b987f583375b037e0792572c2f70537e3889
MakosM11 <mark.makos@stud.uni-corvinus.hu>
2022-10-21 11:01:41 +0200

    Diagram

--- projectweek05/projectweek05/Form1.cs
+++ projectweek05/projectweek05/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using projectweek05.MnbServiceReference;
 using projectweek05.Entities;
 using System.Xml;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace projectweek05
 {
@@ -63,17 +64,33 @@ namespace projectweek05
                 var rate = new RateData();
                 Rates.Add(rate);
 
-                rate.Date = DateTime.Parse(element.GetAttribute("Date"));
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
 
                 var childElement = (XmlElement)element.ChildNodes[0];
                 rate.currency = childElement.GetAttribute("curr");
 
-                var unit = decimal.Parse(childElement.GetAttribute("Unit"));
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
                 var value = decimal.Parse(childElement.InnerText);
                 if (unit != 0)
                     rate.Value = value / unit;
 
+                //Diagram
 
+                chartRateData.DataSource = Rates;
+
+                var series = chartRateData.Series[0];
+                series.ChartType = SeriesChartType.Line;
+                series.XValueMember = "Date";
+                series.YValueMembers = "Value";
+                series.BorderWidth = 2;
+
+                var legend = chartRateData.Legends[0];
+                legend.Enabled = false;
+
+                var chartArea = chartRateData.ChartAreas[0];
+                chartArea.AxisX.MajorGrid.Enabled = false;
+                chartArea.AxisY.MajorGrid.Enabled = false;
+                chartArea.AxisY.IsStartedFromZero = false;
 
             }
 
