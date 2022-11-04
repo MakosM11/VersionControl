0eafa473802ebc30591e46300fc61d7298c10b87
MakosM11 <mark.makos@stud.uni-corvinus.hu>
2022-11-04 10:51:52 +0100

    car

--- Week06_futoszalag/Week06_futoszalag/Entities/Ball.cs
+++ Week06_futoszalag/Week06_futoszalag/Entities/Ball.cs
@@ -5,10 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Week06_futoszalag.Abstractions;
 
 namespace Week06_futoszalag.Entities
 {
-    public class Ball : Label
+    public class Ball : Toy
     {
         public Ball()
         {
@@ -23,7 +24,7 @@ namespace Week06_futoszalag.Entities
             DrawImage(e.Graphics);
         }
 
-        protected void DrawImage(Graphics g)
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
