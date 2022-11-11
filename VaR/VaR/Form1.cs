42573595557394310c38b1c9c44bd7344d6c50d5
MakosM11 <mark.makos@stud.uni-corvinus.hu>
2022-11-11 10:34:20 +0100

    Create

---
+++ VaR/VaR/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VaR
+{
+    public partial class Form1 : Form
+    {
+        PortfolioEntities1 context = new PortfolioEntities1();
+        List<Tick> Ticks;
+        public Form1()
+        {
+            InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
