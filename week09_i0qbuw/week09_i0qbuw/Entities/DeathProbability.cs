c65b84e47cff37d3b7c6c79d6dcecc0390ba8b2b
MakosM11 <mark.makos@stud.uni-corvinus.hu>
2022-12-02 10:36:26 +0100

    Classes

---
+++ week09_i0qbuw/week09_i0qbuw/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week09_i0qbuw.Entities
+{
+    public class DeathProbability
+    {
+        public int Sex { get; set; }
+        public int Age { get; set; }
+        public double Probability { get; set; }
+    }
+}
