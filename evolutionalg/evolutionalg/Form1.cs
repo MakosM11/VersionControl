e73bc835a7d93e6bdf65c9c71c0d6ad6409201a7
MakosM11 <mark.makos@stud.uni-corvinus.hu>
2022-12-09 10:28:08 +0100

    Gen

--- evolutionalg/evolutionalg/Form1.cs
+++ evolutionalg/evolutionalg/Form1.cs
@@ -16,12 +16,60 @@ namespace evolutionalg
         GameController gc = new GameController();
         GameArea ga;
 
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
+
+
         public Form1()
         {
             InitializeComponent();
 
+            label1.Text = "1. Generáció";
+
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+
+            gc.GameOver += Gc_GameOver;
+
+            
+            gc.Start();
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
         }
 
         private void Form1_Load(object sender, EventArgs e)
